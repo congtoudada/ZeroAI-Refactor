eb0f3ab9ca0fdd029aeee022e289ffe383ddbe5d
聪头的游戏世界 <1322080797@qq.com>
2024-07-26 23:43:06 +0800

    Squashed commit of the following:
    commit cc8708b4d76443a3bf1739f80130332a2e9a278f
    Author: 聪头的游戏世界 <1322080797@qq.com>
    Date:   Fri Jul 26 23:41:55 2024 +0800
    
        优化

--- modules/business/phone/phone_comp.py
+++ modules/business/phone/phone_comp.py
@@ -28,21 +28,24 @@ class PhoneComponent(BasedStreamComponent):
 
     def __init__(self, shared_memory, config_path: str):
         super().__init__(shared_memory)
-        self.config: PhoneInfo = PhoneInfo(ConfigKit.load(config_path))
-        self.pname = f"[ {os.getpid()}:phone for {self.config.input_ports[0]}&{self.config.input_ports[1]} ]"
-        self.cam_id = 0
-        self.stream_width = 0
-        self.stream_height = 0
+        self.config: PhoneInfo = PhoneInfo(ConfigKit.load(config_path))  # 配置信息
+        self.pname = f"[ {os.getpid()}:phone for {self.config.input_ports[0]}&{self.config.input_ports[1]} ]"  # 进程唯一标识
+        self.cam_id = 0  # 对应的摄像头id
+        self.stream_width = 0  # 画面宽
+        self.stream_height = 0  # 画面高
         # key: obj_id value: cls
-        self.pool: ObjectPool = ObjectPool(20, PhoneItem)
-        self.record_pool: ObjectPool = ObjectPool(20, DetectionRecord)
-        self.data_dict: Dict[int, PhoneItem] = {}
-        self.border = 10
-        self.tracker: BytetrackHelper = BytetrackHelper(self.config.stream_mot_config)  # 追踪器
+        self.pool: ObjectPool = ObjectPool(20, PhoneItem)  # PhoneItem对象池
+        self.record_pool: ObjectPool = ObjectPool(20, DetectionRecord)  # 检测记录对象池
+        self.data_dict: Dict[int, PhoneItem] = {}  # 有效对象字典
+        self.tracker: BytetrackHelper = BytetrackHelper(self.config.stream_mot_config)  # 人的追踪器
         self.phone_records: List[DetectionRecord] = []  # 手机目标检测结果（每帧更新）
         self.current_mot = None  # 当前帧人的追踪结果，如果非None，则最后要消耗掉检测结果
 
     def on_start(self):
+        """
+        初始化时执行
+        :return:
+        """
         super().on_start()
         self.cam_id = self.read_dict[0][StreamKey.STREAM_CAM_ID.name]
         self.stream_width = self.read_dict[0][StreamKey.STREAM_WIDTH.name]
@@ -50,7 +53,7 @@ class PhoneComponent(BasedStreamComponent):
 
     def on_update(self) -> bool:
         self.release_unused()  # 清理无用资源（一定要在最前面调用）
-        super().on_update()
+        super().on_update()  # 核心更新逻辑
         # 后处理，如果存在人，则清空当前帧的检测结果
         if self.current_mot is not None:
             self.phone_records.clear()
@@ -58,6 +61,11 @@ class PhoneComponent(BasedStreamComponent):
         return True
 
     def on_resolve_per_stream(self, read_idx):
+        """
+        解析每个流的数据（帧、帧id，目标检测结果）
+        :param read_idx:
+        :return:
+        """
         frame, _ = super().on_resolve_per_stream(read_idx)  # 解析视频帧id+视频帧
         if frame is None:  # 没有有效帧
             return frame, None
@@ -68,7 +76,7 @@ class PhoneComponent(BasedStreamComponent):
 
     def on_process_per_stream(self, idx, frame, input_det):
         """
-        处理视频流
+        处理每个流的数据
         :param idx: 从input_ports[idx]取package
         :param frame: 帧
         :param input_det: 目标检测结果
@@ -77,7 +85,7 @@ class PhoneComponent(BasedStreamComponent):
         if input_det is None:
             return None
 
-        if idx == 0:  # 手机
+        if idx == 0:  # 0号端口取的数据是手机检测结果
             for i in range(len(self.phone_records)):
                 self.record_pool.push(self.phone_records[i])
             self.phone_records.clear()  # 清空手机检测记录
@@ -89,30 +97,30 @@ class PhoneComponent(BasedStreamComponent):
                 record.init(ltrb, score, cls)
                 self.phone_records.append(record)
             return None
-        else:  # 人（此时手机记录已经填充）
+        else:  # 1号端口取的是人的检测结果
             mot_result = self.tracker.inference(input_det)  # 返回对齐输出后的mot结果
-            self.current_mot = mot_result
-            # 根据mot结果进行手机检测
+            self.current_mot = mot_result  # 缓存追踪结果（主要用于帧结束时判断是否消耗掉检测结果）
+            # 根据mot结果进行手机核心业务！！！
             self._phone_core(frame, mot_result, self.frame_id_cache[0], frame.shape[1], frame.shape[0])
             return mot_result
 
     def _phone_core(self, frame, input_mot, current_frame_id, width, height) -> bool:
         if input_mot is None:
             return
-        # 时间换精度: 根据t排序（y轴排序），配合包围盒匹配，可以使精度更高
+        # 时间换精度: 根据t排序（y轴排序），配合包围盒匹配，可以使精度更高（默认关闭）
         if self.config.phone_y_sort:
             sort_indices = np.argsort(input_mot[:, 1])
             input_mot = input_mot[sort_indices]
             self.phone_records.sort(key=lambda x: x.ltrb[1])
-        # 遍历每个人
+        # 遍历每个人的追踪结果
         for i, obj in enumerate(input_mot):
             ltrb = obj[:4]
             # 只有在检测区域内才匹配
             if not self._is_in_zone(ltrb, self.config.phone_zone):
                 continue
-            # 包围盒匹配（满足就返回）
+            # 策略一：包围盒匹配，手机在人里面才匹配（满足就返回，贪婪匹配）
             # match_idx = MatchRecordHelper.match_bbox(ltrb, self.phone_records)
-            # 距离匹配（锁定上半身）
+            # 策略二：距离匹配，谁离手机近匹配谁，有最大距离限制（全局匹配）
             w = ltrb[2] - ltrb[0]
             h = ltrb[3] - ltrb[1]
             match_idx = MatchRecordHelper.match_distance_l2(ltrb + (0, 0, 0, -h/2),
@@ -120,8 +128,8 @@ class PhoneComponent(BasedStreamComponent):
             if match_idx != -1:  # 存在匹配项
                 obj_id = int(obj[6])
                 phone = self.phone_records[match_idx]
-                # 手机在人里面，更新人的状态
-                if not self.data_dict.__contains__(obj_id):  # 没有被记录过
+                # 更新人的状态
+                if not self.data_dict.__contains__(obj_id):  # 没有被记录过，则记录
                     item = self.pool.pop()
                     item.init(obj_id, phone.cls, current_frame_id)
                     self.data_dict[obj_id] = item
@@ -129,13 +137,13 @@ class PhoneComponent(BasedStreamComponent):
                     self.data_dict[obj_id].update(current_frame_id, phone.cls)
                 # 计算结果
                 self.process_result(frame, self.data_dict[obj_id], ltrb)  # 满足异常条件就记录
-                # 匹配过的record需标记
+                # 匹配过的record需标记，避免反复匹配
                 phone.match_action(obj_id)
 
     def process_result(self, frame, phone_item: PhoneItem, ltrb):
         # 没有报过警且异常状态保持一段时间才发送
         if not phone_item.has_warn and phone_item.get_valid_count() > self.config.phone_valid_count:
-            if phone_item.cls == 0:  # 持有手机
+            if phone_item.cls == 0:  # 持有手机，报警！
                 logger.info(f"手机检测异常: obj_id:{phone_item.obj_id} cls:{phone_item.cls}")
                 phone_item.has_warn = True  # 一旦视为异常，则一直为异常，避免一个人重复报警
                 shot_img = ImgKit_img_box.draw_img_box(frame, ltrb)
@@ -155,6 +163,13 @@ class PhoneComponent(BasedStreamComponent):
             self.data_dict.pop(key)  # 从字典中移除item
 
     def on_draw_vis(self, idx, frame, input_mot):
+        """
+        可视化
+        :param idx:
+        :param frame:
+        :param input_mot:
+        :return:
+        """
         if input_mot is None:  # 检测手机的端口，不显示任何内容
             return None
         text_scale = 2
