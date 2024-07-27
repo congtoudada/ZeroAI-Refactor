4cc45ebf944eef560a6fed43559cfb88aa50989a
聪头的游戏世界 <1322080797@qq.com>
2024-07-27 15:32:38 +0800

    phone http适配

--- modules/business/phone/phone_comp.py
+++ modules/business/phone/phone_comp.py
@@ -13,6 +13,7 @@ from business.common.match_record_helper import MatchRecordHelper
 from business.phone.phone_info import PhoneInfo
 from business.phone.phone_item import PhoneItem
 from bytetrack.zero.component.bytetrack_helper import BytetrackHelper
+from simple_http.simple_http_helper import SimpleHttpHelper
 from zero.core.component.based_stream_comp import BasedStreamComponent
 from zero.core.helper.warn_helper import WarnHelper
 from zero.core.key.detection_key import DetectionKey
@@ -44,6 +45,7 @@ class PhoneComponent(BasedStreamComponent):
         self.current_mot = None  # 当前帧人的追踪结果，如果非None，则最后要消耗掉检测结果
         self.timing_record = float('-inf')  # reid相关
         self.warn_person_bboxes = []  # 报警人的包围框
+        self.http_helper = SimpleHttpHelper(self.config.stream_http_config)  # http帮助类
 
     def on_start(self):
         """
@@ -163,7 +165,7 @@ class PhoneComponent(BasedStreamComponent):
                 phone_item.has_warn = True  # 一旦视为异常，则一直为异常，避免一个人重复报警
                 self.warn_person_bboxes.append(ltrb)  # 报警人的包围框
                 shot_img = ImgKit_img_box.draw_img_box(frame, ltrb)
-                WarnHelper.send_warn_result(self.pname, self.output_dir[0], self.cam_id, 1, 1,
+                self.http_helper.send_warn_result(self.pname, self.output_dir[0], self.cam_id, 1, 1,
                                             shot_img, self.config.stream_export_img_enable,
                                             self.config.stream_web_enable)
 
